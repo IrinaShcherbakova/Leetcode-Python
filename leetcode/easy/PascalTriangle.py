cae9bd3d7d86fa0ac899d3ac430c08113d8845c2
Irina Shcherbakova <irina-070@bk.ru>
2020-03-07 17:39:26 +0100

    pascal triangle

--- leetcode/easy/PascalTriangle.py
+++ leetcode/easy/PascalTriangle.py
@@ -16,4 +16,17 @@ def generate(numRows):
         currentRow += 1
     return triangle
 
-print("res is %s" % generate(5))
+
+def getRow(rowIndex):
+    previousRow = []
+    for i in range(0, rowIndex+1):
+        newRow = [None for _ in range(i+1)]
+        newRow[0] = newRow[-1] = 1
+        for j in range(1, i):
+            newRow[j] = previousRow[j-1] + previousRow[j]
+        previousRow = newRow
+    return previousRow
+
+
+
+print("res is %s" % getRow(0))
