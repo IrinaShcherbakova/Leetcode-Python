e65953b5a7f1c27f1659fdfab70f3800c3dd3396
Irina <isa530@student.vu.nl>
2020-03-22 19:50:59 +0100

    contains duplicates

--- leetcode/easy/containsDuplicates.py
+++ leetcode/easy/containsDuplicates.py
@@ -8,3 +8,15 @@ def containsDuplicate(self, nums: List[int]) -> bool:
             return True
         uniqueValues.add(nums[i])
     return False
+
+
+def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    uniqueEl = {}
+    for i in range(len(nums)):
+        if nums[i] in uniqueEl:
+            if i - uniqueEl[nums[i]] <= k:
+                return True
+            uniqueEl[nums[i]] = i
+        else:
+            uniqueEl[nums[i]] = i
+    return False
