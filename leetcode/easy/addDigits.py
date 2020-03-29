3fbc724c12e8e16ec61d2680accae381fb09ad52
Irina <isa530@student.vu.nl>
2020-03-29 11:35:36 +0200

    add digits of a number

---
+++ leetcode/easy/addDigits.py
@@ -0,0 +1,16 @@
+class Solution:
+#     def addDigits(self, num: int) -> int:
+#         while len(str(num)) > 1:
+#             num = self.addDigitsHelper(num)
+#         return num
+    
+#     def addDigitsHelper(self, num: int) -> int:
+#         res = 0
+#         for ch in str(num):
+#             res += int(ch)
+#         return res
+
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + ((num - 1) % 9)
