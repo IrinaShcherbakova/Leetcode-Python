5d7c5ef090ef4edaa9e9a1cbe4fafe24cd937dd1
IrinaShcherbakova <irina-070@bk.ru>
2020-06-13 15:52:48 +0200

    design a hashset

---
+++ leetcode/easy/hashset.py
@@ -0,0 +1,31 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.arr = [None] * 1000001
+    
+          
+    def add(self, key: int) -> None:
+        self.arr[key] = key
+        
+            
+    def remove(self, key: int) -> None:
+        self.arr[key] = None
+        
+    
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        if self.arr[key] is not None:
+            return True
+        return False
+        
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
