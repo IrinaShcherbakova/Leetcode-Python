714a264e514faaa2631f98be7b25fb7b42f4e8f5
IrinaShcherbakova <irina-070@bk.ru>
2020-06-14 18:21:00 +0200

    add and search word containing regex

---
+++ leetcode/easy/addSearchWord.py
@@ -0,0 +1,89 @@
+class TrieNode: 
+    
+    def __init__(self, val: str):
+        self.val = val
+        self.children = [None] * 26
+        self.leaf = False
+        
+    def makeLeaf(self) -> None:
+        self.leaf = True
+        
+    def isLeaf(self) -> bool:
+        return self.leaf
+    
+    
+
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode(None)
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        cur = self.root
+        for char in word:
+            if cur.children[ord(char)-97] == None:
+                cur.children[ord(char)-97] = TrieNode(char)
+            cur = cur.children[ord(char)-97]
+        cur.makeLeaf()
+        
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        cur = self.root
+        variants = []
+        for i, char in enumerate(word):
+            if char == '.':
+                for child in cur.children:
+                    if child is not None:
+                        variants.append(child)
+                return self.foundMatchingWord(variants, word[i+1:len(word)], cur)
+            elif cur.children[ord(char)-97] is None:
+                return False
+            elif i + 1 == len(word):
+                return cur.children[ord(char)-97].isLeaf()
+            cur = cur.children[ord(char)-97]
+        return True
+        
+    def foundMatchingWord(self, variants: List[TrieNode], word: str, root: TrieNode) -> bool:
+        for path in variants:
+            if self.traverseTrie(path, word):
+                return True
+        return False
+                
+        
+    def traverseTrie(self, root: TrieNode, word: str) -> bool:
+        if len(word) == 0:
+            return root.isLeaf()
+        cur = root
+        newVariants = []
+        for i, char in enumerate(word):
+            if char == '.':
+                for child in cur.children:
+                    if child is not None:
+                        newVariants.append(child)
+                return self.foundMatchingWord(newVariants, word[i+1:len(word)], cur)
+            elif cur.children[ord(char)-97] is None:
+                return False
+            elif i + 1 == len(word):
+                return cur.children[ord(char)-97].isLeaf()
+            cur = cur.children[ord(char)-97]
+        return False
+        
+        
+        
+        
+        
+        
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
