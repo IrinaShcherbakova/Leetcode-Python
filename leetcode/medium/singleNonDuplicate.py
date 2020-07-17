1098eaf449b97efbeb82886bab23d534f19c087c
IrinaShcherbakova <irina-070@bk.ru>
2020-07-17 14:28:45 +0200

    single element in a sorted array

---
+++ leetcode/medium/singleNonDuplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums)-1:
+            if nums[i] != nums[i+1]:
+                return nums[i]
+            i += 2
+        return nums[i]
