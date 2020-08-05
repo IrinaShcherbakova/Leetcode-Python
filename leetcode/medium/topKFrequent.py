4f7fe15f47daf8761991768015d7601ad82d8cfc
IrinaShcherbakova <irina-070@bk.ru>
2020-08-05 11:49:01 +0200

    top K frequent words

--- leetcode/medium/topKFrequent.py
+++ leetcode/medium/topKFrequent.py
@@ -14,4 +14,35 @@ class Solution:
             if count == k:
                 return res
         return res
+               class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        frequency = {}
+        for word in words:
+            if word in frequency:
+                frequency[word] += 1
+            else:
+                frequency[word] = 1
+        
+        groups_with_same_freq = {}
+        for word in frequency:
+            freq = frequency[word] 
+            if freq in groups_with_same_freq:
+                groups_with_same_freq[freq].append(word)
+            else:
+                groups_with_same_freq[freq] = [word]
                 
+        count = 0
+        res = []
+        for freq in sorted(groups_with_same_freq, reverse = True):
+            words = groups_with_same_freq[freq]
+            words.sort()
+            if count + len(words) < k:
+                res += words
+                count += len(words)
+            else:
+                res += words[:k-count]
+                return res
+        return res
+            
+        
+             
