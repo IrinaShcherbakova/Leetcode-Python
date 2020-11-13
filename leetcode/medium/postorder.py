f8df64352f8d480d8f40ab24f1455d9779ab08e0
IrinaShcherbakova <irina-070@bk.ru>
2020-11-13 10:08:10 +0100

    binary tree iterative postorder traversal

---
+++ leetcode/medium/postorder.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if not root:
+            return res
+        stack = [root]
+        while stack:
+            cur = stack.pop()
+            res.append(cur.val)
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+        return res[::-1]
