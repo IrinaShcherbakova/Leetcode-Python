1b304d226bbe4d9ece1ffe2d9ffbe9817b46ef39
IrinaShcherbakova <irina-070@bk.ru>
2020-12-01 21:19:33 +0100

    maximum number of nodes from root to leaves

---
+++ leetcode/monthly-challenges/maxDepth.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        # print("in node %s, left = %s, right = %s" %(root.val,left,right))
+        return max(left, right) + 1
